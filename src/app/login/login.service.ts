5908edf1a72e453a97363e827a39a60e2bb80a0d
Patroklos <enzo-renzo@hotmail.com>
2024-01-08 10:20:51 -0500

    Add routing /chat in app.routes

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,34 +1,33 @@
 // debugger;
-import { Injectable } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
-import { BehaviorSubject, Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {Router, RouterModule} from '@angular/router';
+import {BehaviorSubject, Observable} from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: 'root',
 })
 export class LoginService {
-  static KEY = 'username';
+	static KEY = 'username';
 
-  private username = new BehaviorSubject<string | null>(null);
+	private username = new BehaviorSubject<string | null>(null);
 
-  constructor(private router:Router) {
-    this.username.next(localStorage.getItem(LoginService.KEY));
-  }
+	constructor(private router: Router) {
+		this.username.next(localStorage.getItem(LoginService.KEY));
+	}
 
-  login(login: { username: string; password: string }) {
-    console.log("login entro")
-    localStorage.setItem(LoginService.KEY, login.username)
-    this.username.next(localStorage.getItem(LoginService.KEY));
-    this.router.navigate(['/', 'chat']);
-  }
+	login(login: { username: string; password: string }) {
+		localStorage.setItem(LoginService.KEY, login.username)
+		this.username.next(localStorage.getItem(LoginService.KEY))
+		this.router.navigate(['/chat'])
+	}
 
-  logout() {
-    localStorage.removeItem(LoginService.KEY)
-    this.username.next(null);
-    this.router.navigate(['']);
-  }
+	logout() {
+		localStorage.removeItem(LoginService.KEY)
+		this.username.next(null)
+		this.router.navigate([''])
+	}
 
-  getUsername(): Observable<string | null> {
-    return this.username.asObservable();
-  }
+	getUsername(): Observable<string | null> {
+		return this.username.asObservable();
+	}
 }
