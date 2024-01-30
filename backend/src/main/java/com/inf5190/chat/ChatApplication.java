b1d3497df55d54c8b95718dc62475b96da65ac4c
Patroklos <enzo-renzo@hotmail.com>
2024-01-29 20:50:36 -0500

    Add Firebase connection and dependencies

--- backend/src/main/java/com/inf5190/chat/ChatApplication.java
+++ backend/src/main/java/com/inf5190/chat/ChatApplication.java
@@ -1,26 +1,38 @@
 package com.inf5190.chat;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 
-import com.inf5190.chat.auth.filter.AuthFilter;
-
-import com.inf5190.chat.auth.session.SessionDataAccessor;
-import com.inf5190.chat.auth.session.SessionManager;
-
-import java.util.Optional;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 /**
  * Application spring boot.
  */
 @SpringBootApplication
 public class ChatApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ChatApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(ChatApplication.class, args);
+        try {
+            if (FirebaseApp.getApps().isEmpty()) {
+                FileInputStream serviceAccount = new FileInputStream("/home/kaped/IdeaProjects/5190_Messaging_Platform_1/backend/firebase-key.json");
+                FirebaseOptions options = FirebaseOptions.builder()
+                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                        .build();
+                LOGGER.info("Initializing Firebase application.");
+                FirebaseApp.initializeApp(options);
+            }
+            LOGGER.info("Firebase application already initialized.");
+            SpringApplication.run(ChatApplication.class, args);
+        } catch (IOException e) {
+            System.err.println("Could not initialise application. Please check your service account key path");
+        }
     }
-
 }
