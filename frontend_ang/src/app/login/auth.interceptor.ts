4d2365615caed22f1d8828ab2c401a26782620eb
Patroklos <enzo-renzo@hotmail.com>
2024-02-17 15:11:01 -0500

    Rollback Interceptor to given default

--- frontend_ang/src/app/login/auth.interceptor.ts
+++ frontend_ang/src/app/login/auth.interceptor.ts
@@ -1,26 +1,23 @@
-import {Injectable} from '@angular/core';
-import {
-	HttpInterceptor,
-	HttpEvent,
-	HttpRequest,
-	HttpHandler, HTTP_INTERCEPTORS,
-} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {LoginService} from './login.service';
-
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpEvent, HttpRequest, HttpHandler, } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { LoginService } from './login.service';
 @Injectable()
-export class MyHttpInterceptor implements HttpInterceptor{
-	constructor(private loginService : LoginService) {
-	}
-
-	intercept(req: HttpRequest<any>, next: HttpHandler):
-		Observable<HttpEvent<any>> {
-		const token = this.loginService.getToken();
-		const modifiedReq = req.clone({
-			url: req.url,
-			headers: req.headers.set('Authorization', `Bearer ${token}`),
-		});
-
-		return next.handle(modifiedReq);
-	}
+export class MyHttpInterceptor implements HttpInterceptor {
+  constructor(private loginService: LoginService) {}
+  intercept(
+    httpRequest: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const token = this.loginService.getToken();
+    if (token) {
+      httpRequest = httpRequest.clone({
+        url: httpRequest.url,
+        setHeaders: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    }
+    return next.handle(httpRequest);
+  }
 }
