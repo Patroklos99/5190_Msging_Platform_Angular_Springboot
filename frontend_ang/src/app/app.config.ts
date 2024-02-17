e7b371eabada8c0bc8101707567ae1bffcee5b76
Patroklos <enzo-renzo@hotmail.com>
2024-02-17 15:02:55 -0500

    Fix  auth.interceptor Not Intercepting http request to add Header (Bearer and Token)

--- frontend_ang/src/app/app.config.ts
+++ frontend_ang/src/app/app.config.ts
@@ -1,14 +1,14 @@
 import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptors, withInterceptorsFromDi} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
 import {MyHttpInterceptor} from "./login/auth.interceptor";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 export const appConfig: ApplicationConfig = {
   providers: [
       provideRouter(routes),
-      provideHttpClient(),
+      provideHttpClient(withInterceptorsFromDi()),
       { provide : HTTP_INTERCEPTORS, useClass : MyHttpInterceptor, multi : true },
       importProvidersFrom([BrowserAnimationsModule])
   ],
